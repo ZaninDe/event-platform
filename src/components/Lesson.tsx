b53f8af35d5ba5ea463e8c27f63e75b9da157fef
GabrielZanin1997 <gabriel.zaninde@gmail.com>
2022-06-22 14:32:58 -0300

    fetching lessons from graphcms and show with date formated

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,25 +1,46 @@
-import { CheckCircle } from 'phosphor-react'
+import { CheckCircle, Lock } from 'phosphor-react'
+import { isPast, format } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
 
-export function Lesson() {
+interface LessonProps {
+  title: string;
+  slug: string;
+  availableAt: Date;
+  type: 'live' | 'class';
+}
+
+export function Lesson(props: LessonProps) {
+  const isLessonAvailable = isPast(props.availableAt)
+  const availabeDateFormated = format(props.availableAt, "EEEE' • 'd' de 'MMMM' • 'k'h'MM", {
+    locale: ptBR,
+  })
   return (
     <a href="#">
       <span className='text-gray-300'>
-        Terça • 22 de junho • 19h00
+        {availabeDateFormated}
       </span>
 
       <div className='rounded border border-gray-500 p-4 mt-2'>
         <header className="flex items-center justify-between">
-          <span className='text-sm text-blue-500 font-medium flex items-center gap-2' >
-            <CheckCircle size={20}/>
-            Conteúdo liberado
-          </span>
+          {isLessonAvailable ? (
+            <span className='text-sm text-blue-500 font-medium flex items-center gap-2' >
+              <CheckCircle size={20} />
+              Conteúdo liberado
+            </span>
+          ) : (
+            <span className='text-sm text-orange-500 font-medium flex items-center gap-2' >
+              <Lock size={20} />
+              Em breve
+            </span>
+          )
+          }
           <span className='text-xs rounded px-2 py-[0.125rem] text-white border border-green-300 font-bold'>
-            AO VIVO
+            {props.type === 'live' ? 'AO VIVO' : 'AULA PRÁTICA'}
           </span>
         </header>
 
         <strong className='text-gray-200 mt-5 block'>
-          Abertura do evento Ignite labs
+          {props.title}
         </strong>
       </div>
     </a>
