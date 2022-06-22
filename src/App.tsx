b99f7cea8f859bc409fc3c402fb47cdada0a0d86
GabrielZanin1997 <gabriel.zaninde@gmail.com>
2022-06-22 13:57:19 -0300

    lesson static component created

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,20 @@
 import { gql, useQuery } from "@apollo/client"
 import { useEffect } from "react"
+import { Header } from "./components/Header"
+import { Lesson } from "./components/Lesson"
+import { Sidebar } from "./components/Sidebar"
+import { Video } from "./components/Video"
 import { client } from "./lib/apollo"
+import { Event } from "./pages/Event"
 
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-    }
-  }
-`
 
-interface Lesson {
-  id: string;
-  title: string;
-}
+
 
 function App() {
-  const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY)
-  console.log(data)
   return (
-    <ul>
-      {data?.lessons.map(lesson => {
-        return (
-          <li key={lesson.id}>{lesson.title}</li>
-        )
-      })}
-    </ul>
+    <>
+     <Event />
+    </>
   )
 }
 
