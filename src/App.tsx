28e58c85cd278405d4d863d0b5a407a03c7e4556
GabrielZanin1997 <gabriel.zaninde@gmail.com>
2022-06-27 20:35:32 -0300

    create subscribe

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { ApolloProvider } from '@apollo/client';
 import { Event } from './pages/Event';
 import { client } from './lib/apollo';
 import { BrowserRouter } from 'react-router-dom';
-import { Router } from './components/Router';
+import { Router } from './Router';
 
 function App() {
   return (
